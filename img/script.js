4845599c1c2a9e2b56971c9e86b5a0751566b720
yuriTXC <yurin@acad.ifma.edu.br>
2025-07-10 21:28:29 -0300

    Delete img directory

--- img/script.js
+++
@@ -1,8 +0,0 @@
-//Código provisório para o menu (que não deu certo)
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav_links");
-
-burger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
-
